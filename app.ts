9a816ff6e4891dbc7cf8dea4ed9274959f1fb1da
TheRookie100 <121271205+TheRookie100@users.noreply.github.com>
2023-04-08 12:19:36 -0300

    Desafio 1 metodos de depositar e sacar verificando se conta esta ativa e verificando se valor do saldo

---
+++ app.ts
@@ -0,0 +1,28 @@
+import { CompanyAccount } from './class/CompanyAccount'
+import { DioAccount } from './class/DioAccount'
+import { PeopleAccount } from './class/PeopleAccount'
+
+/*
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
+peopleAccount.deposit_(100)
+console.log(peopleAccount)
+
+const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+companyAccount.deposit_(3600)
+console.log(companyAccount)
+*/
+
+//                                                id   nome   n▫ conta
+const account1: PeopleAccount = new PeopleAccount(1 , "Joe", 1000)
+console.log(account1) //  toda informacao da conta com seus atributos e metodos
+account1.deposit_(9000) // valor depositado
+account1.withdraw_(150) // valor sacado
+console.log(account1.balance) // saldo atual
+
+
+const account2: PeopleAccount = new PeopleAccount(23 , "Luccas" , 639)
+console.log(account2) //  toda informacao da conta com seus atributos e metodos
+account2.deposit_(40000) // valor depositado
+account2.withdraw_(2500) // valor sacado
+console.log(account2.balance) // saldo atual
+
---
+++ class/DioAccount.ts
@@ -0,0 +1,55 @@
+export abstract class DioAccount {
+  private name: string
+  private readonly accountNumber: number
+  balance: number = 0
+  private status: boolean = true
+
+  constructor(name: string, accountNumber: number){
+    this.name = name
+    this.accountNumber = accountNumber
+  }
+
+  setName = (name: string): void => {
+    this.name = name
+    console.log('Nome alterado com sucesso!')
+  }
+
+  getName = (): string => {
+    return this.name
+  }
+
+  
+  //              ⬇ valor a ser depositado
+  deposit_ = (amount:number): void => {
+    if(this.validateStatus() && amount >0){ // verficar a codicao da conta se esta ativa e valor depositado e maior que zero
+      console.log("Voce depositou um valor na sua conta!" , + amount)
+      
+      this.balance = amount
+      console.log("Seu saldo atual:" , + this.balance);
+      
+    } 
+  }
+  //             ⬇ valor a ser sacado
+  withdraw_ = (amount: number): void => {
+    if(this.validateStatus() && this.balance >= amount) { // verficar a codicao da conta se esta ativa e valor do saldo e maior ou igual ao solicitado para saque
+      console.log("Você sacou um valor na sua conta!", amount );
+      this.balance -= amount;
+      console.log("Seu saldo atual:");
+    }
+  }
+
+
+  getBalance = (): void => {
+    console.log(this.balance)
+  }
+
+  
+
+  private validateStatus = (): boolean => {
+    if (this.status) {
+      return this.status
+    }
+
+    throw new Error('Conta inválida')
+  }
+}
